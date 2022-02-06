003c2d6ce6c07b3b0605c845fbdab97dc97d6af5
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-02-06 19:55:10 +0700

    add main layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import './assets/css/index.css';
+import './assets/css/grid.css';
+import PrefectureSelector from './components/prefectureSelector/PrefectureSelector';
+import LineChart from './components/chart/LineChart';
+
 function App() {
-    return <div>This is my app</div>;
+    return (
+        <div className='container'>
+            <div className='header'>
+                日本の管理単位による人口の変化を示しているチャート
+            </div>
+            <PrefectureSelector />
+            <LineChart />
+        </div>
+    );
 }
 
 export default App;
