478afe2e82911bba68e213af5ed688ee8bbcb2ed
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-02-06 19:55:22 +0700

    add constants

---
+++ src/common/constants.js
@@ -0,0 +1,10 @@
+// Api url base
+export const apiUrl = 'https://opendata.resas-portal.go.jp/api/v1';
+
+// RESAS api key
+export const X_API_KEY = '5hvrCiTBFR2OzYKQkVj5ztPcU5LDEjjSfKhQEaQt';
+
+// Prefecture action type
+export const SET_CHECK = 'SET_CHECK';
+export const SET_PREF_ALL = 'SET_PREF_ALL';
+export const SET_PREF_CHECK = 'SET_PREF_CHECK';
